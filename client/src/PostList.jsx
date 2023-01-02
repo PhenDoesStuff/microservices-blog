cb2c49815847a660e864841aafb2af0200fd21f2
Stephen Montague <Stephen.D.Montague@gmail.com>
2023-01-02 18:43:53 -0500

    Created the create comment component and rendered it inside of the appropriate place.

--- client/src/PostList.jsx
+++ client/src/PostList.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { useEffect } from 'react';
+import CommentCreate from './CommentCreate';
 
 const PostList = () => {
 	const [posts, setPosts] = useState({});
@@ -25,6 +26,7 @@ const PostList = () => {
 				key={post.id}>
 				<div className='card-body'>
 					<h3>{post.title}</h3>
+					<CommentCreate postId={post.id} />
 				</div>
 			</div>
 		);
