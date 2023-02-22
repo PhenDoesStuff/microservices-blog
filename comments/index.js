ac0327d8bc60fbc8313f246f379bc6c9d8a8c6df
Stephen Montague <Stephen.D.Montague@gmail.com>
2023-02-21 22:00:47 -0500

    Added logic to do the comment moderation and then update the comment.

--- comments/index.js
+++ comments/index.js
@@ -41,8 +41,36 @@ app.post('/posts/:id/comments', async (req, res, next) => {
 	res.status(201).send(comments);
 });
 
-app.post('/events', (req, res, next) => {
+app.post('/events', async (req, res, next) => {
 	console.log('Received Event', req.body.type);
+
+	const { type, data } = req.body;
+
+	if (type === 'CommentModerated') {
+		const { id, postId, status } = data;
+
+		const comments = commentsByPostId[postId];
+
+		const comment = comments.find(comment => {
+			return comment.id === id;
+		});
+
+		comment.status = status;
+
+		try {
+			await axios.post('http://localhost:4005', {
+				type: 'CommentUpdated',
+				data: {
+					id,
+					status,
+					postId,
+					content,
+				},
+			});
+		} catch (err) {
+			return next(err);
+		}
+	}
 
 	res.status(200);
 });
